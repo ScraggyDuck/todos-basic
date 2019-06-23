9f77c6134fdf7aa74021841cf5f29a6210d28d8f
JokerV <Quocvietcoi2000@gmail.com>
2019-06-23 17:42:04 +0700

    Updates

---
+++ precache-manifest.8911af6ad64e87557e2fadf569143fd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95d801611e753b440dbd696de379c5c",
+    "url": "/todos-basic/index.html"
+  },
+  {
+    "revision": "565a79911f144df67611",
+    "url": "/todos-basic/static/css/main.b7568d8f.chunk.css"
+  },
+  {
+    "revision": "0ca85bad117c804348d5",
+    "url": "/todos-basic/static/js/2.d89bab35.chunk.js"
+  },
+  {
+    "revision": "565a79911f144df67611",
+    "url": "/todos-basic/static/js/main.8b2e86c2.chunk.js"
+  },
+  {
+    "revision": "c6f4928c9dbd09de9c01",
+    "url": "/todos-basic/static/js/runtime~main.13b6a2be.js"
+  },
+  {
+    "revision": "d29f1779664178a55ed87236fd97c302",
+    "url": "/todos-basic/static/media/check-complete.d29f1779.svg"
+  },
+  {
+    "revision": "40eb9cc6d51d63d1dfd8a0ea29b8d700",
+    "url": "/todos-basic/static/media/check.40eb9cc6.svg"
+  },
+  {
+    "revision": "b55208ceebe374f5bedb0889ef2c0a4b",
+    "url": "/todos-basic/static/media/delete-item.b55208ce.svg"
+  },
+  {
+    "revision": "38966f6df73b74ee8af3e5ced57c4a49",
+    "url": "/todos-basic/static/media/tick.38966f6d.svg"
+  }
+]);
